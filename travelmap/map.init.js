9392d752513c33fc31808334b0ce40937c534a62
daleonpz <daleonpz@gmail.com>
2017-06-28 15:29:10 -0500

    does not depend on jquery

--- travelmap/map.init.js
+++ travelmap/map.init.js
@@ -34,7 +34,7 @@ jQuery(function ($) {
     var container = $("#map");
     var r = Raphael('map', container.width(), container.height());
     var panZoom = r.panzoom({ initialZoom: 5, initialPosition: { x: 120, y: 300} });
-	var isHandling = false;
+    var isHandling = false;
 	
     panZoom.enable();
     r.safari();
@@ -61,31 +61,33 @@ jQuery(function ($) {
     for (var country in paths){
         var obj = r.path(paths[country].path);
 
-        obj.click(handleDetails);
+   //     obj.click(handleDetails);
         obj.data({
             'code': paths[country].code, 
             'name': paths[country].name 
         });
         
         colorize(obj, attr_visit, attr_no_visit);
-        obj.hover(animateOver, animateOut);
+//         obj.hover(animateOver, animateOut);
+        obj.mouseover(animateOver);
+        obj.mouseout(animateOut);
         arr[paths[country].name] = obj;
     }
 
-    $("#mapContainer #up").click(function (e) {
-        panZoom.zoomIn(1);
-        e.preventDefault();
-    });
-
-    $("#mapContainer #down").click(function (e) {
-        panZoom.zoomOut(1);
-        e.preventDefault();
-    });
-    
-    $("#others #moveTopLeft").click(function (e) {
-        panZoom.pan(1,1);
-    });
- 
+//     $("#mapContainer #up").click(function (e) {
+//         panZoom.zoomIn(1);
+//         e.preventDefault();
+//     });
+// 
+//     $("#mapContainer #down").click(function (e) {
+//         panZoom.zoomOut(1);
+//         e.preventDefault();
+//     });
+//     
+//     $("#others #moveTopLeft").click(function (e) {
+//         panZoom.pan(1,1);
+//     });
+//  
     function animateOver() {
             this.attr("opacity",0.7) ;
         if( this.attr("fill") == "#111111")
@@ -102,44 +104,44 @@ jQuery(function ($) {
                     "Somewhere over the ocean";
     }
     
-    function handleDetails() {
-        if (panZoom.isDragging() || isHandling) return;
-        isHandling = true;
-        var anim, box = this.getBBox();
-
-        if (inDetails) {
-            inDetails = false;
-            panZoom.enable();
-            this.hover(animateOver, animateOut);
-            anim = overlay.animate({ 'fill-opacity': 0 }, 300, function () { this.toBack(); isHandling = false; });
-            this.animateWith(overlay, anim, {
-                transform: ""
-            }, 300);
-            this.attr("fill", this.data("fill"));
-        }
-        else {
-            inDetails = true;
-            panZoom.disable();
-            this.unhover(animateOver, animateOut);
-            overlay.toFront();
-            this.toFront();
-
-            var currPaperPosition = panZoom.getCurrentPosition();
-            var currPaperZoom = panZoom.getCurrentZoom();
-
-            var currHeight = r.height * (1 - currPaperZoom * 0.1);
-
-            var zoomDif = (currHeight / 2) / box.height;
-
-            var xdif = currPaperPosition.x - box.x + ((box.width * zoomDif) - box.width) / 2;
-            var ydif = (currPaperPosition.y + ((currHeight / 2) - (box.height / 2))) - box.y;
-
-
-            anim = overlay.animate({ 'fill-opacity': 0.7 }, 300, function () { isHandling = false; });
-            this.animateWith(overlay, anim, {
-                transform: "t" + xdif + "," + ydif + "s" + zoomDif
-            }, 300);
-        }
-    }
+//     function handleDetails() {
+//         if (panZoom.isDragging() || isHandling) return;
+//         isHandling = true;
+//         var anim, box = this.getBBox();
+// 
+//         if (inDetails) {
+//             inDetails = false;
+//             panZoom.enable();
+//             this.hover(animateOver, animateOut);
+//             anim = overlay.animate({ 'fill-opacity': 0 }, 300, function () { this.toBack(); isHandling = false; });
+//             this.animateWith(overlay, anim, {
+//                 transform: ""
+//             }, 300);
+//             this.attr("fill", this.data("fill"));
+//         }
+//         else {
+//             inDetails = true;
+//             panZoom.disable();
+//             this.unhover(animateOver, animateOut);
+//             overlay.toFront();
+//             this.toFront();
+// 
+//             var currPaperPosition = panZoom.getCurrentPosition();
+//             var currPaperZoom = panZoom.getCurrentZoom();
+// 
+//             var currHeight = r.height * (1 - currPaperZoom * 0.1);
+// 
+//             var zoomDif = (currHeight / 2) / box.height;
+// 
+//             var xdif = currPaperPosition.x - box.x + ((box.width * zoomDif) - box.width) / 2;
+//             var ydif = (currPaperPosition.y + ((currHeight / 2) - (box.height / 2))) - box.y;
+// 
+// 
+//             anim = overlay.animate({ 'fill-opacity': 0.7 }, 300, function () { isHandling = false; });
+//             this.animateWith(overlay, anim, {
+//                 transform: "t" + xdif + "," + ydif + "s" + zoomDif
+//             }, 300);
+//         }
+//     }
     
 });
