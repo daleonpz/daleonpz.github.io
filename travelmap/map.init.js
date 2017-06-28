3cde2139e31c9da04fa63406a7d932dc1f89cdc6
daleonpz <daleonpz@gmail.com>
2017-06-28 09:40:33 -0500

    add visited countries

--- travelmap/map.init.js
+++ travelmap/map.init.js
@@ -1,16 +1,33 @@
+function colorize(obj,attr1, attr2){
+    switch(obj.data('code')){
+        case "US":
+        case "CA": 
+            obj.attr(attr1);
+            break;
+        default:
+            obj.attr(attr2);
+    }
+}
+
 jQuery(function ($) {
     var inDetails = false;
     var container = $("#map");
     var r = Raphael('map', container.width(), container.height());
-//     var r = Raphael(0,0,1200,800);
     var panZoom = r.panzoom({ initialZoom: 5, initialPosition: { x: 120, y: 300} });
 	var isHandling = false;
 	
     panZoom.enable();
     r.safari();
 
-    var attributes = {
+    var attr_no_visit = {
         fill: '#cccccc',
+        stroke: '#FFFFFF',
+        'stroke-width': 2,
+        'stroke-linejoin': 'round'
+    };
+    
+    var attr_visit = {
+        fill: '#111111',
         stroke: '#FFFFFF',
         'stroke-width': 2,
         'stroke-linejoin': 'round'
@@ -24,12 +41,18 @@ jQuery(function ($) {
     for (var country in paths){
         var obj = r.path(paths[country].path);
 
-        obj.attr(attributes);
         obj.click(handleDetails);
         obj.data({
             'code': paths[country].code, 
             'name': paths[country].name 
         });
+        
+        colorize(obj, attr_visit, attr_no_visit);
+//         if (obj.data('code') == 'US')
+//             obj.attr(attr_visit);
+//         else
+//             obj.attr(attr_no_visit);
+// 
         obj.hover(animateOver, animateOut);
         arr[paths[country].name] = obj;
     }
@@ -47,7 +70,7 @@ jQuery(function ($) {
     $("#others #moveTopLeft").click(function (e) {
         panZoom.pan(1,1);
     });
-   
+ 
     function animateOver() {
             this.attr("opacity",0.7) ;
             document.getElementById('country-name').innerHTML =
